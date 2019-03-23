bb611a0bab4efbd42953548204d78f7ea0a655b9
theyurry <kubrican.juraj@gmail.com>
2019-03-23 17:19:52 +0100

    swagger mock

---
+++ src/main/kotlin/sk/knet/dp/endpointshell/MockRoutes.kt
@@ -0,0 +1,14 @@
+package sk.knet.dp.endpointshell
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+class Register {
+    @GetMapping("register")
+    fun register() {
+
+    }
+}
+
